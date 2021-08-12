8929f18565d4556aa2507e007cd4486e0e1237ea
Cristian Morales <cristian.morales@rappi.com>
2021-08-11 21:42:46 -0500

    adding validations to the operators buttons

--- app/src/main/java/com/cmorales/mycalculator/MainActivity.kt
+++ app/src/main/java/com/cmorales/mycalculator/MainActivity.kt
@@ -3,15 +3,57 @@ package com.cmorales.mycalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var lastNumeric = false
+    var lastDot = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
    }
 
     fun onDigit(view: View) {
-        Toast.makeText(this, "button works", Toast.LENGTH_SHORT).show()
+        tvInput.append((view as Button).text)
+        lastNumeric = true
+
+        if(tvInput.text.contains("")){
+
+        }
+    }
+
+    fun onClear(view: View) {
+        tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimalPoint(view: View) {
+        if(lastNumeric && !lastDot) {
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+    fun onOperator(view: View) {
+        if (lastNumeric && !isOperatorAdded(tvInput.text.toString())) {
+            tvInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false
+        }
+    }
+
+    private fun isOperatorAdded(value: String) : Boolean {
+        return if (value.startsWith("-")) {
+            false
+        } else {
+            value.contains("/" ) ||
+            value.contains("*" ) ||
+            value.contains("+" ) ||
+            value.contains("-" )
+        }
     }
 }
